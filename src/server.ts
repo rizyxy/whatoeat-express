ec17e0937c1d7c77cec8979dcec83fdc2c4423ec
Mukhammad Rizki <rizkyRE7@gmail.com>
2025-10-19 10:01:09 +0700

    Created Get Ingredient Route

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,29 @@
 import express from 'express';
+import { PrismaClient } from '../generated/prisma';
 
 const app = express();
+app.use(express.json());
+
 const port = 3000
+
+const prisma = new PrismaClient();
+
+app.get('/ingredients', async (req, res) => {
+    try {
+        const ingredients = await prisma.ingredient.findMany({
+            orderBy: {
+                id: 'asc'
+            }
+            });
+
+        res.json(ingredients);
+    } catch (error) {
+        res.status(500).json({
+            error: error
+        });
+    }
+});
+
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
