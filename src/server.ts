81c9774f086814b18bef6c338f12dfb455a70709
Mukhammad Rizki <rizkyRE7@gmail.com>
2025-11-02 17:25:23 +0700

    Added cors

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,13 @@
-import express from "express"
+import express from "express";
+import cors from "cors";
 import ingredientRouter from "./routes/ingredient.route";
 
 const app = express();
 app.use(express.json());
+
+app.use(cors({
+    origin: 'localhost'
+}));
 
 const port = 3000;
 
