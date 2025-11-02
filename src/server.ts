cafadcbc803a1f9e0cf76a0743535ff78beb9b45
Mukhammad Rizki <rizkyRE7@gmail.com>
2025-11-02 16:18:27 +0700

    Called Prisma Client

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
 import express from "express"
+import { PrismaClient } from "../generated/prisma/client";
 
 const app = express();
 app.use(express.json());
+
+const prisma = new PrismaClient();
 
 const port = 3000;
 
